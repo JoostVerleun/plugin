8441405eb0edbce7a04203fec0ac2080bd19bad8
JoostVerleun <66355868+JoostVerleun@users.noreply.github.com>
2021-07-08 09:41:19 +0200

    Add files via upload

---
+++ Chaos/src/main/java/com/events/NiceXpEvent.java
@@ -0,0 +1,20 @@
+package com.netlify.hparcells.chaos.events;
+
+import com.netlify.hparcells.chaos.Chaos;
+import com.netlify.hparcells.chaos.ChaosEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+public class NiceXpEvent extends ChaosEvent {
+    public void onEnable(String username, Chaos plugin) {
+        Player target = (Bukkit.getServer().getPlayer(username));
+
+        if(target != null) {
+            target.setLevel(69);
+        }
+    }
+
+    public String getName() {
+        return "Nice XP";
+    }
+}
